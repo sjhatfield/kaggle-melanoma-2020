182feb4eba80d86db3632a1c60796408b5cc15bb
sjhatfield <s.j.hatfield@live.co.uk>
2020-08-12 08:51:40 +0100

    first commit

---
+++ src/data/prepare_data.py
@@ -0,0 +1,280 @@
+import numpy as np
+import pandas as pd
+import PIL
+from PIL import Image, ImageEnhance, ImageOps, ImageDraw
+from torch.utils.data import Dataset
+from torchvision import transforms
+from pathlib import Path
+import random
+import torch
+
+
+class UniformAugment:
+    """
+    Contains a number of image augmentations that may be chosen
+    uniformly at random during training and testing. The default
+    fill color is grey
+    """
+
+    def __init__(self, operations: int = 2, fillcolor: tuple = (128, 128, 128)):
+        self.operations = operations
+        self.augs = {
+            "shearX": [-0.3, 0.3],
+            "shearY": [-0.3, 0.3],
+            "translateX": [-0.45, 0.45],
+            "translateY": [-0.45, 0.45],
+            "rotate": [-30, 30],
+            "autocontrast": [0, 0],
+            "invert": [0, 0],
+            "equalize": [0, 0],
+            "solarize": [0, 256],
+            "posterize": [4, 8],
+            "contrast": [0.1, 1.9],
+            "color": [0.1, 1.9],
+            "brightness": [0.1, 1.9],
+            "sharpness": [0.1, 1.9],
+            "cutout": [0, 0.2],
+        }
+
+        def rotate_with_fill(img: PIL.Image, degrees: float):
+            """
+            Rotates the image by magnitude (degrees counter 
+            clockwise) and then fills the gaps with defaul
+            color
+            """
+            rot = img.convert("RGBA").rotate(degrees)
+            return Image.composite(
+                rot, Image.new("RGBA", rot.size, (128,) * 4), rot
+            ).convert(img.mode)
+
+        def cutout(img, magnitude: float, fillcolor: tuple):
+            """
+            Cuts out a square from the image and fills 
+            with the default color
+            """
+            img = img.copy()
+            w, h = img.size
+            v = w * magnitude
+            x0 = np.random.uniform(w)
+            y0 = np.random.uniform(h)
+            x0 = int(max(0, x0 - v / 2.0))
+            y0 = int(max(0, y0 - v / 2.0))
+            x1 = min(w, x0 + v)
+            y1 = min(h, y0 + v)
+            xy = (x0, y0, x1, y1)
+            ImageDraw.Draw(img).rectangle(xy, fillcolor)
+            return img
+
+        """
+        These are all the possible augmentations of the image,
+        most of which are straight from the PIL package
+        """
+        self.func = {
+            "shearX": lambda img, magnitude: img.transform(
+                img.size,
+                Image.AFFINE,
+                (1, magnitude, 0, 0, 1, 0),
+                Image.BICUBIC,
+                fillcolor=fillcolor,
+            ),
+            "shearY": lambda img, magnitude: img.transform(
+                img.size,
+                Image.AFFINE,
+                (1, 0, 0, magnitude, 1, 0),
+                Image.BICUBIC,
+                fillcolor=fillcolor,
+            ),
+            "translateX": lambda img, magnitude: img.transform(
+                img.size, Image.AFFINE, (1, 0, magnitude, 0, 1, 0), fillcolor=fillcolor
+            ),
+            "translateY": lambda img, magnitude: img.transform(
+                img.size, Image.AFFINE, (1, 0, 0, 0, 1, magnitude), fillcolor=fillcolor
+            ),
+            "rotate": lambda img, magnitude: rotate_with_fill(img, magnitude),
+            "color": lambda img, magnitude: ImageEnhance.Color(img).enhance(magnitude),
+            "posterize": lambda img, magnitude: ImageOps.posterize(img, int(magnitude)),
+            "solarize": lambda img, magnitude: ImageOps.solarize(img, int(magnitude)),
+            "contrast": lambda img, magnitude: ImageEnhance.Contrast(img).enhance(
+                magnitude
+            ),
+            "sharpness": lambda img, magnitude: ImageEnhance.Sharpness(img).enhance(
+                magnitude
+            ),
+            "brightness": lambda img, magnitude: ImageEnhance.Brightness(img).enhance(
+                magnitude
+            ),
+            "autocontrast": lambda img, magnitude: ImageOps.autocontrast(img),
+            "equalize": lambda img, magnitude: ImageOps.equalize(img),
+            "invert": lambda img, magnitude: ImageOps.invert(img),
+            "cutout": lambda img, magnitude: cutout(
+                img, magnitude, fillcolor=fillcolor
+            ),
+        }
+
+    def __call__(self, img):
+        """
+        Select a random sample of augmentations where 
+        self.operations determines how many and perform them
+        on the image
+        """
+        operations = random.sample(list(self.augs.items()), self.operations)
+        for operation in operations:
+            augmentation, range = operation
+            # Uniformly select value from range of augmentations
+            magnitude = random.uniform(range[0], range[1])
+            probability = random.random()
+            # Perform augmentation uniformly at random
+            if random.random() < probability:
+                img = self.func[augmentation](img, magnitude)
+        return img
+
+
+class ImageTransform:
+    """
+    Image transformations to be done which depend on what
+    phase of learning is taking place. Normalization happens
+    first
+    """
+
+    def __init__(
+        self,
+        resize: int,
+        uniform_augment: UniformAugment,
+        mean: tuple = (0.485, 0.456, 0.406),  # ImageNet
+        std: tuple = (0.229, 0.224, 0.225),  # ImageNet
+        train: bool = True,
+    ):
+        self.data_transform = {
+            "train": transforms.Compose(
+                [
+                    transforms.RandomResizedCrop(size=resize, scale=(0.7, 1.0)),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.RandomVerticalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean, std),
+                ]
+            ),
+            "valid": transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize(mean, std)]
+            ),
+            "test": transforms.Compose(
+                [
+                    transforms.RandomResizedCrop(size=resize, scale=(0.7, 1.0)),
+                    transforms.RandomHorizontalFlip(),
+                    transforms.RandomVerticalFlip(),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean, std),
+                ]
+            ),
+        }
+        if uniform_augment:
+            self.data_transform["train"].transforms.insert(0, UniformAugment())
+            self.data_transform["test"].transforms.insert(0, UniformAugment())
+
+    def __call__(self, img, phase):
+        return self.data_transform[phase](img=img)
+
+
+class MelanomaDataset(Dataset):
+    def __init__(
+        self,
+        base_dir: str,
+        info_dataframe: pd.DataFrame,
+        transform=None,
+        phase: str = "train",
+    ):
+        self.base_dir = base_dir
+        self.info = info_dataframe
+        self.transform = transform
+        self.phase = phase
+
+    def __len__(self):
+        return len(self.info)
+
+    def __getitem__(self, index):
+        p = Path(self.base_dir, self.info.loc[index, "image_name"] + ".jpg")
+        img = Image.open(p)
+        img_transformed = self.transform(img, self.phase)
+        if self.phase in ["train", "valid"]:
+            return {
+                "inputs": img_transformed,
+                "labels": torch.tensor(
+                    self.info.loc[index, "target"], dtype=torch.int64
+                ),
+            }
+        else:
+            return {
+                "inputs": img_transformed,
+            }
+
+
+class MelanomaDataset(Dataset):
+    def __init__(
+        self,
+        base_dir: str,
+        info_dataframe: pd.DataFrame,
+        transform=None,
+        phase: str = "train",
+        external: bool = False,
+    ):
+        self.base_dir = base_dir
+        self.info = info_dataframe
+        self.transform = transform
+        self.phase = phase
+
+    def __len__(self):
+        return len(self.info)
+
+    def __getitem__(self, index):
+        if self.phase == "test":
+            p = Path(self.base_dir, self.info.loc[index, "image_name"] + ".jpg")
+        elif not self.info.loc[index, "tfrecord"] < 15:
+            p = Path(self.base_dir, self.info.loc[index, "image_name"] + ".jpg")
+        # If tfrecord is >=15 then this indicates data is external
+        else:
+            p = Path(
+                "../data/external/train", self.info.loc[index, "image_name"] + ".jpg"
+            )
+        img = Image.open(p)
+        img_transformed = self.transform(img, self.phase)
+        if self.phase in ["train", "valid"]:
+            return {
+                "inputs": img_transformed,
+                "labels": torch.tensor(
+                    self.info.loc[index, "target"], dtype=torch.int64
+                ),
+            }
+        else:
+            return {
+                "inputs": img_transformed,
+            }
+
+
+def format_tabular(
+    train_raw: pd.DataFrame, test_raw: pd.DataFrame = None
+) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Takes in the dataframes containing all metadata and dummifies the categories and removes the
+    filenames and patientnames before returning X and y to be fit
+    """
+    train_raw.loc[:, "age_approx"].fillna(0, inplace=True)
+    train_raw.loc[:, "anatom_site_general_challenge"].fillna("NA", inplace=True)
+    train = train_raw[["sex", "age_approx", "width", "height"]].copy()
+    train["sex"] = train["sex"].apply(lambda x: 1.0 if x == "male" else 0.0)
+    train.loc[:, "sex"].fillna(-1, inplace=True)
+    train_dummies = pd.get_dummies(data=train_raw["anatom_site_general_challenge"])
+    train = pd.concat([train, train_dummies], axis=1)
+
+    if type(test_raw) == pd.DataFrame:
+        test_raw.loc[:, "age_approx"].fillna(0, inplace=True)
+        test_raw.loc[:, "anatom_site_general_challenge"].fillna("NA", inplace=True)
+        test = test_raw[["sex", "age_approx", "width", "height"]].copy()
+        test["sex"] = test["sex"].apply(lambda x: 1.0 if x == "male" else 0.0)
+        test.loc[:, "sex"].fillna(-1, inplace=True)
+        test_dummies = pd.get_dummies(data=test_raw["anatom_site_general_challenge"])
+        test = pd.concat([test, test_dummies], axis=1)
+
+        return train, test, train_raw["target"]
+
+    return train, train_raw["target"]
+
---
+++ src/models/model.py
@@ -0,0 +1,286 @@
+import sys
+
+sys.path.append("..")
+
+from sklearn.metrics import roc_auc_score
+from torchvision import models
+from efficientnet_pytorch import EfficientNet
+import torch
+from torch import nn
+from src.data.prepare_data import MelanomaDataset
+from torch.utils.data import DataLoader
+from typing import Union
+import time
+from tqdm import tqdm
+import pandas as pd
+
+
+class AverageMeter:
+    """
+    Keeps track of some summary statistics for the losses when
+    training.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def load_model(model: str, out_features: int):
+    """
+    Loads the model and sets the correct number of out
+    features. Then requires gradients for all but the last 
+    layer so that backpropogation can take place
+    """
+    net = None
+    if model.startswith("efficientnet"):
+        net = EfficientNet.from_pretrained(model, num_classes=out_features)
+        for name, param in net.named_parameters():
+            if "_fc" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+    elif model == "vgg19":
+        net = models.vgg19(pretrained=True)
+        net.classifier[6] = nn.Linear(
+            in_features=net.classifier[6].weight.size(1), out_features=out_features
+        )
+        for name, param in net.named_parameters():
+            if name in ["classifier.6.weight", "classifier.6.bias"]:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+    elif model == "resnext":
+        net = models.resnext101_32x8d(pretrained=True)
+        net.fc = nn.Linear(in_features=net.fc.weight.size(1), out_features=out_features)
+        for name, param in net.named_parameters():
+            if "fc" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+    elif model == "densenet":
+        net = models.densenet161(pretrained=True)
+        net.classifier = nn.Linear(
+            in_features=net.classifier.weight.size(1), out_features=out_features
+        )
+        for name, param in net.named_parameters():
+            if "classifier" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+    # The next two are small networks for running locally. All
+    # others are too large to run locally
+
+    elif model == "resnet18":
+        net = models.resnet18(pretrained=True)
+        net.fc = nn.Linear(in_features=net.fc.weight.size(1), out_features=out_features)
+        for name, param in net.named_parameters():
+            if "fc" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+    elif model == "alexnet":
+        net = models.alexnet(pretrained=True)
+        for name, param in net.named_parameters():
+            if "classifier.6" in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+    return net
+
+
+def eval_model(
+    model: torch.nn,
+    dataset: MelanomaDataset,
+    batch_size: int,
+    criterion: torch.nn,
+    num_workers: int,
+    device=None,
+):
+    """
+    Evaluates the model by passing through images and finding the loss and 
+    AUC_ROC score to assess performance
+    """
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers
+    )
+    losses = AverageMeter()
+    preds = []
+    targets = []
+    with torch.no_grad():
+        for data in dataloader:
+            inputs = data["inputs"].to(device)
+            labels = data["labels"].to(device)
+
+            # forward
+            outputs = model(inputs)
+
+            # loss
+            loss = criterion(outputs, labels)
+            losses.update(loss.item(), inputs.size(0))
+
+            # prediction
+            preds.append(outputs[:, 1])
+            targets.append(labels)
+
+        # auc
+        preds = torch.cat(preds, dim=-1).cpu().numpy()
+        targets = torch.cat(targets, dim=-1).cpu().numpy()
+        auc = roc_auc_score(targets, preds)
+
+    return losses.avg, auc
+
+
+def train_model(
+    model_id: str,
+    dataset_train: MelanomaDataset,
+    dataset_valid: MelanomaDataset,
+    batch_size: int,
+    model: torch.nn,
+    criterion: torch.nn,
+    optimizer: torch.nn,
+    scheduler: torch.optim,
+    num_epochs: int,
+    freezed_epochs: int,
+    base_dir: str,
+    num_workers: int,
+    sampler: torch.utils.data.sampler,
+    device: str = Union["cuda", "cpu"],
+    early_stopping: int = 3,
+):
+
+    model.to(device)
+
+    # create a dataloader
+    if sampler != None:
+        dataloader_train = DataLoader(
+            dataset_train,
+            batch_size=batch_size,
+            sampler=sampler,
+            num_workers=num_workers,
+        )
+    else:
+        dataloader_train = DataLoader(
+            dataset_train, batch_size=batch_size, shuffle=True, num_workers=num_workers
+        )
+
+    # train
+    for epoch in range(1, num_epochs + 1):
+        losses = AverageMeter()
+        s_time = time.time()
+
+        if epoch == freezed_epochs + 1:
+            # unfreeze upstream layers
+            model.load_state_dict(
+                torch.load(f"../models/state_dict_{model_id}.pt", map_location=device)
+            )
+            for param in model.parameters():
+                param.requires_grad = True
+            for g in optimizer.param_groups:
+                g["lr"] = 1e-4
+
+        # set the train mode
+        model.train()
+
+        for data in tqdm(dataloader_train):
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            inputs = data["inputs"].to(device)
+            labels = data["labels"].to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            losses.update(loss.item(), inputs.size(0))
+
+        # set the eval mode
+        model.eval()
+
+        # calculate validation loss and auc
+        loss_valid, auc_valid = eval_model(
+            model, dataset_valid, batch_size, criterion, num_workers, device
+        )
+
+        # save the checkpoint
+        if epoch == 1 or auc_valid > max_auc:
+            saved = True
+            max_auc = auc_valid
+            torch.save(model.state_dict(), f"../models/state_dict_{model_id}.pt")
+            counter = 0
+        else:
+            saved = False
+            counter += 1
+
+        # Save on each epoch with the AUC so that if training fails it can be restarted
+        # with the best performing loaded
+        torch.save(
+            model.state_dict(),
+            f"../models/state_dict_{model_id}_epoch:_{epoch}_auc_{round(auc_valid, 2)}.pt",
+        )
+
+        # print statistics
+        e_time = time.time()
+        print(
+            f"epoch: {epoch}, loss_train: {losses.avg:.4f}, loss_valid: {loss_valid:.4f}, auc_valid: {auc_valid:.4f}, saved: {saved}, {(e_time - s_time):.4f}sec"
+        )
+
+        # no operation in freezed epochs
+        if epoch < freezed_epochs + 1:
+            counter = 0
+        else:
+            # step the scheduler
+            scheduler.step(auc_valid)
+
+            # early stopping
+            if early_stopping != None:
+                if counter == early_stopping:
+                    break
+
+
+def predict(dataset, batch_size, model, device=None):
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    model.to(device)
+    model.eval()
+    preds = []
+    with torch.no_grad():
+        for data in dataloader:
+            inputs = data["inputs"].to(device)
+            outputs = model(inputs)
+            preds.append(outputs)
+        preds = torch.cat(preds)
+
+    return preds
+
+
+def get_predictions(
+    dataset: MelanomaDataset,
+    batch_size: int,
+    model: torch.nn,
+    test_time_augmentations: int,
+    predictions: pd.DataFrame,
+    device: str = None,
+):
+    for _ in tqdm(range(test_time_augmentations)):
+        pred_test = predict(dataset, batch_size, model, device)
+        pred_test = pd.DataFrame(torch.softmax(pred_test, 1)[:, 1].numpy())
+        predictions = pd.concat([predictions, pred_test], axis=1)
+    return predictions
